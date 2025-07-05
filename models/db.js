32bf0eba04afcd1eabacde2cf880d748b85fab4b
Rahman <abdullah384858@gmail.com>
2025-07-05 17:35:16 +0530

    adding mongourl_url

--- models/db.js
+++ models/db.js
@@ -1,30 +1,13 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     // await mongoose.connect('mongodb+srv://rehman:1234567891@cluster0.yk51c2c.mongodb.net/employee?retryWrites=true&w=majority&appName=Cluster0');
-//     await mongoose.connect('mongodb+srv://abdullah384858:rahman%40123@cluster0.dmpovik.mongodb.net/employee?retryWrites=true&w=majority&appName=Cluster0');
-//     console.log(" Database connection successful");
-//   } catch (error) {
-//     console.error(" Database connection failed:", error);
-//   }
-// };
-
-// module.exports = connectDB;
-
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require('dotenv').config(); // Make sure this is at the top
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb+srv://abdullah384858:rahman384858@cluster0.dmpovik.mongodb.net/employee?retryWrites=true&w=majority&appName=Cluster0', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("Database connection successful");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.log(`Something went wrong: ${error}`);
+    }
 };
 
 module.exports = connectDB;
